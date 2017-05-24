6abfc0f5db66d47015e49d88a94b7a6d144dfcc8
DTG1995 <tiengioiit@gmail.com>
2017-05-25 00:58:05 +0700

    update page home 25052017

--- WA_Blogers_MVC/Controllers/HomeController.cs
+++ WA_Blogers_MVC/Controllers/HomeController.cs
@@ -3,13 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WA_Blogers_MVC.Models;
 
 namespace WA_Blogers_MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private  WA_BlogerEntities db = new WA_BlogerEntities();
         public ActionResult Index()
         {
+            ViewBag.ListTopNews = db.WA_Posts.OrderBy(x => x.Created).Take(5).ToList();
+            ViewBag.ListPostFeatured = db.WA_Posts.OrderBy(x => x.Seen).Take(8).ToList();
             return View();
         }
 
