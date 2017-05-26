25a996dfdf179400b7879d012138bf5f1f34fea9
DTG1995 <tiengioiit@gmail.com>
2017-05-27 01:05:16 +0700

    push to merge ttnd

--- WA_Blogers_MVC/Controllers/AdminController.cs
+++ WA_Blogers_MVC/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 using WA_Blogers_MVC.Models;
 namespace WA_Blogers_MVC.Controllers
 {
@@ -15,12 +16,46 @@ namespace WA_Blogers_MVC.Controllers
         {
             return View();
         }
-        public JsonResult ChangeTopNav(List<string> listNameTopNav, List<string> listUrlTopNav){
+        public JsonResult ChangeTopNav(List<string> listNameTopNav, List<string> listUrlTopNav, List<string> listSocialName, List<string> listSocialUrl)
+        {
             var list = db.WA_Options.Where(x=>x.Group.Equals("Topnav") || x.Group.Equals("TopnavSocical"));
             db.WA_Options.RemoveRange(list);
             db.SaveChanges();
+            var  listOptionNav =new List<WA_Options>();
+            if (listNameTopNav != null)
+            {
+                for (int i = 0; i < listNameTopNav.Count; i++)
+                {
+                    string link = listUrlTopNav[i] ?? "#";
+                    if (link.Length > 4 && !link.Substring(0, 4).Equals("http") && link != "#")
+                        link = "http://" + link;
+                    listOptionNav.Add(new WA_Options
+                    {
+                        Group = "Topnav",
+                        Name = listNameTopNav[i],
+                        Value = link
+                    });
+                }
+            }
+            if (listSocialName != null)
+            {
+                for (int i = 0; i < listSocialName.Count; i++)
+                {
+                    string link = listSocialUrl[i] ?? "#";
+                    if (link.Length > 4 && !link.Substring(0, 4).Equals("http") && link != "#")
+                        link = "http://" + link;
+                    listOptionNav.Add(new WA_Options
+                    {
+                        Group = "TopnavSocical",
+                        Name = listSocialName[i],
+                        Value = link
+                    });
+                }
+            }
             
-            return Json(list);
+            db.WA_Options.AddRange(listOptionNav);
+            db.SaveChanges();
+            return Json(listOptionNav, JsonRequestBehavior.AllowGet);
 
         }
 	}
