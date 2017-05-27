31f00181ef5cfaad78129a626cf81bcd102b77ec
DTG1995 <tiengioiit@gmail.com>
2017-05-27 12:08:26 +0700

    sang 27-05-2017

--- WA_Blogers_MVC/Controllers/AdminController.cs
+++ WA_Blogers_MVC/Controllers/AdminController.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using PagedList;
 using WA_Blogers_MVC.Models;
+using System.IO;
+using System.Data.Entity;
 namespace WA_Blogers_MVC.Controllers
 {
     public class AdminController : Controller
@@ -16,7 +18,7 @@ namespace WA_Blogers_MVC.Controllers
         {
             return View();
         }
-        public JsonResult ChangeTopNav(List<string> listNameTopNav, List<string> listUrlTopNav, List<string> listSocialName, List<string> listSocialUrl)
+        public JsonResult ChangeTopNav(List<string> listNameTopNav, List<string> listUrlTopNav, List<string> txtUrlAd, List<string> listSocialName, List<string> listSocialUrl)
         {
             var list = db.WA_Options.Where(x=>x.Group.Equals("Topnav") || x.Group.Equals("TopnavSocical"));
             db.WA_Options.RemoveRange(list);
@@ -57,6 +59,83 @@ namespace WA_Blogers_MVC.Controllers
             db.SaveChanges();
             return Json(listOptionNav, JsonRequestBehavior.AllowGet);
 
+        }
+        [HttpPost]
+        public ActionResult ChangeMidleTop(HttpPostedFileBase fileBase, string txtUrlAd, string useClock)
+        {
+            var time = db.WA_Options.Where(x => x.Name.Equals("timeclock")).FirstOrDefault();
+            string imgAds="";
+            string imgLogo="";
+            string path = "~/Public/";
+            foreach (string upload in Request.Files)
+            {
+                
+                if (Request.Files[upload].ContentLength == 0) continue;
+                string pathToSave = Server.MapPath(path);
+                string filename = Path.GetFileName(Request.Files[upload].FileName);
+                Request.Files[upload].SaveAs(Path.Combine(pathToSave, filename));
+                if (upload.Equals("logoPage"))
+                    imgLogo = filename;
+                else imgAds = filename;
+            }
+            if(!String.IsNullOrEmpty(imgLogo))
+            {
+                var logo = db.WA_Options.Where(x => x.Name.Equals("logo")).FirstOrDefault();
+                if(logo != null)
+                {
+                    logo.Value = imgLogo;
+                    db.Entry(logo).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                else
+                {
+                    db.WA_Options.Add(new WA_Options { Name = "logo", Value = imgLogo });
+                    db.SaveChanges();
+                }
+            }
+            if(useClock.Equals("true"))
+            {
+                
+                if(time==null)
+                {
+                    db.WA_Options.Add(new WA_Options { Name = "timeclock", Value = "<div class=\"clock\" style=\"margin:0px;width:460px;float:right;max-height: 90px;\" ></div>" });
+                    db.SaveChanges();
+                }
+            }
+            else
+            {
+                db.WA_Options.Remove(time);
+                db.SaveChanges();
+                var ads = db.WA_Options.Where(x => x.Group.Equals("Ads")).ToList();
+                if (ads != null)
+                {
+                    if(ads.Count<2)
+                    {
+                        db.WA_Options.RemoveRange(ads);
+                        ads.Add(new WA_Options { Name = "AdsLink", Value = txtUrlAd, Group = "Ads" });
+                        ads.Add(new WA_Options { Name = "AdsImage", Value = imgAds, Group = "Ads" });
+                        db.WA_Options.AddRange(ads);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        for (int i = 0; i < 2; i++)
+                        {
+                            if (ads[i].Name == "AdsLink")
+                                ads[i].Value = txtUrlAd;
+                            else ads[i].Value = imgAds;
+                        }
+                    }
+                }
+                else
+                {
+                    ads.Add(new WA_Options { Name = "AdsLink", Value = txtUrlAd, Group = "Ads" });
+                    ads.Add(new WA_Options { Name = "AdsImage", Value = imgAds, Group = "Ads" });
+                    db.WA_Options.AddRange(ads);
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("Index");
         }
 	}
 }
